f9c89b5e9b51a53c4d7b6f783b006e51189ed437
Miguel Veloso <mvelosop@gmail.com>
2021-04-05 18:30:45 +0100

    Add test server and bot-related testing support classes

--- src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/EchoBotTests/EchoBotShould.cs
+++ src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/EchoBotTests/EchoBotShould.cs
@@ -1,11 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace AdaptiveDialogBot.BotApp.IntegrationTests.EchoBotTests
 {
-    public class EchoBotShould
+    public class EchoBotShould : IClassFixture<BotApplicationFactory>, IDisposable
     {
+        private bool _disposedValue;
+        private readonly IServiceScope _scope;
+
+        public EchoBotShould(BotApplicationFactory applicationFactory)
+        {
+            _scope = applicationFactory.CreateScope();
+        }
+
         [Fact]
         public async Task Reply_back_whatever_it_receives()
         {
@@ -15,6 +24,28 @@ namespace AdaptiveDialogBot.BotApp.IntegrationTests.EchoBotTests
 
             // Assert ------------------
 
+        }
+
+        private T GetService<T>() => _scope.ServiceProvider.GetRequiredService<T>();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _scope.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
