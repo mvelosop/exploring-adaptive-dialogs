85d15a775af818cf15d26d48dbb6e8b5d3c338c2
Miguel Veloso <mvelosop@gmail.com>
2021-04-05 22:30:07 +0100

    Get initial Adaptive Dialogs bot running

---
+++ src/services/AdaptiveDialogsBot.BotApp/Bots/DialogBot.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdaptiveDialogsBot.BotApp.Bots
+{
+    public class DialogBot<T> : ActivityHandler
+        where T : Dialog
+    {
+        private readonly DialogManager DialogManager;
+        protected readonly ILogger Logger;
+
+        public DialogBot(T rootDialog, ILogger<DialogBot<T>> logger)
+        {
+            Logger = logger;
+
+            DialogManager = new DialogManager(rootDialog);
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            Logger.LogInformation("Running dialog with Activity.");
+            await DialogManager.OnTurnAsync(turnContext, cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/services/AdaptiveDialogsBot.BotApp/Dialogs/RootDialog/RootDialog.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using System.Collections.Generic;
+
+namespace AdaptiveDialogsBot.BotApp.Dialogs.RootDialog
+{
+    public class RootDialog : AdaptiveDialog
+    {
+        public RootDialog() : base(nameof(RootDialog))
+        {
+            Triggers = new List<OnCondition> {
+                new OnUnknownIntent {
+                    Actions = {
+                        new SendActivity("Hi, we're running with Adaptive Dialogs!"),
+                    }
+                },
+            };
+        }
+    }
+}
