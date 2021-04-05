df956f335847df2852132e98250475c21547e696
Miguel Veloso <mvelosop@gmail.com>
2021-04-05 15:19:51 +0100

    Create test project with basic infrastructure

---
+++ src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/EchoBotTests/EchoBotShould.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdaptiveDialogBot.BotApp.IntegrationTests.EchoBotTests
+{
+    public class EchoBotShould
+    {
+        [Fact]
+        public async Task Reply_back_whatever_it_receives()
+        {
+            // Arrange -----------------
+
+            // Act ---------------------
+
+            // Assert ------------------
+
+        }
+    }
+}
