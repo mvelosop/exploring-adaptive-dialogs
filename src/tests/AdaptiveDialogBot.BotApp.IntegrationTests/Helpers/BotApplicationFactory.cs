f3db185d10a90c49eaed69db6db59c6597d62871
Miguel Veloso <mvelosop@gmail.com>
2021-04-05 16:24:49 +0100

    Add test server and bot-related testing support classes

---
+++ src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/Helpers/BotApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using AdaptiveDialogBot.BotApp.IntegrationTests.Helpers;
+using AdaptiveDialogsBot.BotApp;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AdaptiveDialogBot.BotApp.IntegrationTests.EchoBotTests
+{
+    public class BotApplicationFactory : WebApplicationFactory<Startup>
+    {
+        private bool _serverStarted;
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<TestAdapter, BotApplicationTestAdapter>();
+            });
+
+            return base.CreateHost(builder);
+        }
+
+        public IServiceScope CreateScope()
+        {
+            EnsureServerStarted();
+            return Services.CreateScope();
+        }
+
+        private void EnsureServerStarted()
+        {
+            if (_serverStarted) return;
+
+            CreateClient();
+            _serverStarted = true;
+        }
+    }
+}
