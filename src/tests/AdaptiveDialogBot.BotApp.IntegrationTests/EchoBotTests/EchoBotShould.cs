aac5d5d75404eaa7cd157e90062c206f7416e2b4
Miguel Veloso <mvelosop@gmail.com>
2021-04-05 18:03:23 +0100

    Add test cases

--- src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/EchoBotTests/EchoBotShould.cs
+++ src/tests/AdaptiveDialogBot.BotApp.IntegrationTests/EchoBotTests/EchoBotShould.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
@@ -19,8 +22,45 @@ namespace AdaptiveDialogBot.BotApp.IntegrationTests.EchoBotTests
         public async Task Reply_back_whatever_it_receives()
         {
             // Arrange -----------------
+            var testAdapter = GetService<TestAdapter>();
+            testAdapter.Locale = "en-us";
+
+            var bot = GetService<IBot>();
+            var testFlow = new TestFlow(testAdapter, bot);
 
             // Act ---------------------
+            await testFlow
+                .SendConversationUpdate()
+                .AssertReply("Hello and welcome!")
+                .Send("Echo TEST!")
+                .AssertReply(activity => activity.AsMessageActivity().Text.Should().Be("Echo: Echo TEST!"))
+                .StartTestAsync();
+
+            // Assert ------------------
+
+        }
+
+        [Fact]
+        public async Task Greet_back_on_hello()
+        {
+            // Arrange -----------------
+            var testAdapter = GetService<TestAdapter>();
+            testAdapter.Locale = "en-us";
+
+            var bot = GetService<IBot>();
+            var testFlow = new TestFlow(testAdapter, bot);
+
+            // Act ---------------------
+            await testFlow
+                .SendConversationUpdate()
+                .AssertReply("Hello and welcome!")
+                .Send("Hello")
+                .AssertReplyOneOf(new[] { 
+                    "Good morning",
+                    "Good afternoon",
+                    "Good evening"
+                })
+                .StartTestAsync();
 
             // Assert ------------------
 
