2c044f84f7a03def66745a5bf0ac7dbd29029ef1
Miguel Veloso <mvelosop@gmail.com>
2021-04-11 20:02:17 +0100

    Add Language Generator

--- src/services/AdaptiveDialogsBot.BotApp/Dialogs/RootDialog/RootDialog.cs
+++ src/services/AdaptiveDialogsBot.BotApp/Dialogs/RootDialog/RootDialog.cs
@@ -3,15 +3,25 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Generators;
+using Microsoft.Bot.Builder.LanguageGeneration;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AdaptiveDialogsBot.BotApp.Dialogs.RootDialog
 {
     public class RootDialog : AdaptiveDialog
     {
-        public RootDialog() : base(nameof(RootDialog))
+        public RootDialog(
+            IHostEnvironment hostEnvironment)
+            : base(nameof(RootDialog))
         {
+            var dialogRoot = Path.Combine(hostEnvironment.ContentRootPath, "Dialogs");
+            var templates = Templates.ParseFile(Path.Combine(dialogRoot, "RootDialog", "RootDialog.lg"));
+            Generator = new TemplateEngineLanguageGenerator(templates);
+
             Triggers = new List<OnCondition> {
                 new OnConversationUpdateActivity {
                     Actions = {
@@ -21,7 +31,7 @@ namespace AdaptiveDialogsBot.BotApp.Dialogs.RootDialog
                                 new IfCondition {
                                     Condition = "$foreach.value.id != turn.activity.recipient.id",
                                     Actions = {
-                                        new SendActivity("Hello and welcome!")
+                                        new SendActivity("${Welcome()}")
                                     }
                                 }
                             }
@@ -31,31 +41,32 @@ namespace AdaptiveDialogsBot.BotApp.Dialogs.RootDialog
 
                 new OnUnknownIntent {
                     Actions = {
-                        new CodeAction(async (dialogContext, options) =>
-                        {
-                            string replyText;
+                        new IfCondition {
+                            Condition = "toLower(turn.activity.text) == 'hello'",
 
-                            if (dialogContext.Context.Activity.Text.Equals("Hello", StringComparison.OrdinalIgnoreCase))
-                            {
-                                var now = DateTime.Now.TimeOfDay;
+                            Actions = {
+                                new CodeAction(async (dialogContext, options) =>
+                                {
+                                    var now = DateTime.Now.TimeOfDay;
 
-                                var time = now < new TimeSpan(12, 0, 0)
-                                    ? "morning"
-                                    : now > new TimeSpan(19, 0, 0)
-                                        ? "evening"
-                                        : "afternoon";
+                                    var time = now < new TimeSpan(12, 0, 0)
+                                        ? "morning"
+                                        : now > new TimeSpan(19, 0, 0)
+                                            ? "evening"
+                                            : "afternoon";
 
-                                replyText = "Good " + time;
+                                    dialogContext.State.SetValue("dialog.greetingTime", time);
+
+                                    return await dialogContext.EndDialogAsync(options);
+                                }),
+
+                                new SendActivity("${Greeting()}")
+                            },
+
+                            ElseActions = {
+                                new SendActivity("${Echo()}")
                             }
-                            else
-                            {
-                                replyText = $"Echo: {dialogContext.Context.Activity.Text}";
-                            }
-
-                            await dialogContext.Context.SendActivityAsync(MessageFactory.Text(replyText, replyText));
-
-                            return await dialogContext.EndDialogAsync(options);
-                        }),
+                        },
                     }
                 },
             };
